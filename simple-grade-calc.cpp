df3f156194db9c49988413446b8b75d4007e23a4
honeypatel11 <honeypatel1109@gmail.com>
2024-12-27 10:58:29 +0530

    simple calculate

---
+++ simple-grade-calc.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+    int score;
+    char grade;
+
+    cout << "Enter Your Score : ";
+    cin  >> score;
+
+    grade = (score >= 90) ? 'A' : (score >= 80) ? 'B' : (score >= 70) ? 'c' : (score >= 60) ? 'D' : 'F' ;
+
+    cout << "Your Grade Is :- " << grade << endl;
+
+    switch (grade)
+    {
+        case 'A':
+        cout << "Excellent Work !" << endl;
+        break;
+        case 'B':
+        cout << "Well Done !" <<endl;
+        break;
+        case 'C':
+        cout << "Good Job !" <<endl;
+        break;
+        case 'D' :
+        cout << "You Passed, But You Could Do Better." <<endl;
+        break;
+        case 'F':
+        cout << "Sorry, You Failed." <<endl;
+        default:
+        cout << "Invalid Grade." <<endl;
+        break;
+    }
+
+    if(grade == 'A' || grade == 'B' || grade == 'c')
+
+
+
+    return 0;
+}
